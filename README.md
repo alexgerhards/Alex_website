8c5fa29838bcb738e8547c3024a068afe513c788
Alex Gerhards <alex.gerhards@echostar.com>
2012-06-15 15:06:48 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#When readmes are written things occur
+but are those things good?
+in a recent study by the mvrt group (mayan veritable redirection tessolate)
+it was proven to be that 99% of readmes are not read by more than 3 million monkeys
+given this fact we must increase the volatility of the readme files
+set it up so that when a readme file goes unread for more that 15 seconds it begins to cause the computer to
+leak battery acid onto the hands of its user
+thank you for your consideration
+the readmeking
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '181c2bd07af2481a32bd6ffe79c980ea59520ea00d1887d72d78b4b4a544da0ec0949a80eb58bedbe4a8018aeb0057ed1f3842abb401c126fc129b1177ddf85a'
